67fa0d47f7237d8ab5e1f34ba4d191c80fbc93ea
Ali Zemani <mehot1@gmail.com>
2025-10-24 11:42:50 +0330

    Remove unused console transformation plugin from babel.config.js and package.json to streamline dependencies and improve build performance in the Safarnak project.

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,10 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
-  const prod = process.env.NODE_ENV === 'production';
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      prod && ['transform-remove-console', { exclude: ['error', 'warn'] }],
-    ].filter(Boolean),
   };
 };
