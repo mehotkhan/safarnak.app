f35700f0fa1445dd493c7e63d3e758991759e752
Ali Zemani <mehot1@gmail.com>
2025-10-30 15:33:18 +0330

    feat(graphql): load schema from file; add getTrip/getTrips; make Trip fields nullable

--- graphql/schema-loader.ts
+++ graphql/schema-loader.ts
@@ -1,41 +1,9 @@
 // Read GraphQL schema from .graphql file
 // This is used by the worker to load the schema at runtime
 
-export const readGraphQLSchema = (): string => {
-  // In Cloudflare Workers, we need to read the file differently
-  // For now, we'll inline the schema content
-  // In production, you might want to bundle this differently
+// Load from file at build-time using Wrangler `rules` (Text loader for .graphql)
+// This keeps schema single-sourced in graphql/schema.graphql
+// @ts-expect-error - loader returns a string at build time
+import schemaSource from '@graphql/schema.graphql';
 
-  return `type Query {
-  getMessages: [Message!]!
-  me: User
-}
-
-type Mutation {
-  addMessage(content: String!): Message!
-  register(username: String!, password: String!): AuthPayload!
-  login(username: String!, password: String!): AuthPayload!
-}
-
-type Subscription {
-  newMessages: Message!
-}
-
-type Message {
-  id: ID!
-  content: String!
-  createdAt: String!
-}
-
-type User {
-  id: ID!
-  name: String!
-  username: String!
-  createdAt: String!
-}
-
-type AuthPayload {
-  user: User!
-  token: String!
-}`;
-};
+export const readGraphQLSchema = (): string => schemaSource as string;
